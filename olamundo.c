ad6923e32f32c7875c30cad167a73f824f6625f8
Isaac Rodrigues <rodrigues.contano@gmail.com>
2025-01-30 00:55:25 +0000

    Criando codigo que exibe a mensagem ola mundo na tela

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,12 @@
+// Incluindo a biblioteca padrão de entrada e saída, necessária para usar a função printf
+#include <stdio.h>
 
+// int main() é onde o programa começa a execução. Toda aplicação em C começa pela função main
+int main(){
+    
+    // A linha abaixo imprime uma mensagem na tela. O \n adiciona uma nova linha ao final da mensagem
+    printf("Olá, Mundo!\n");
+
+    // Indica que o programa terminou com sucesso
+    return 0;
+}
